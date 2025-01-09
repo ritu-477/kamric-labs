3588aff894b766c4eb0c438167f4fae473cacd33
ritu-477 <ritujoharbishnoi@gmail.com>
2025-01-09 21:27:36 +0530

    upcoming-projects-done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import UpcomingProjects from './components/UpcomingProjects';
 
 function App() {
   return (
     <>
       <Header />
-      <Hero/>
+      <Hero />
+      <UpcomingProjects/>
     </>
   );
 }
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -4,3 +4,34 @@ export const NAV_DATA_LINK = [
     { data: 'Projects', href: '#projects' },
     { data: 'About', href: '#about' },
 ];
+
+export const UPCOMING_PROJECTS = [
+    {
+        id: 1,
+        image: './assets/images/png/nubbies.png',
+        title: 'Nubbies',
+        time: '15 : 54 : 12',
+        category: 'NFT',
+    },
+    {
+        id: 2,
+        image: './assets/images/png/nekozuma.png',
+        title: 'Nekozuma',
+        time: '15 : 54 : 12',
+        category: 'NFT',
+    },
+    {
+        id: 3,
+        image: './assets/images/png/galactium.png',
+        title: 'Galactium',
+        time: '15 : 54 : 12',
+        category: 'NFT',
+    },
+    {
+        id: 4,
+        image: './assets/images/png/frenz.png',
+        title: 'Frenz Forever',
+        time: '15 : 54 : 12',
+        category: 'NFT',
+    },
+];
